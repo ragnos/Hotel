9901d166407ecd1ff11435483c5f7acc493420f7
ragnos <ragnos@utzone.de>
2014-10-25 16:29:36 +0200

    Zimmer-Klasse

---
+++ src/hotel/zimmer.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotel;
+
+/**
+ *
+ * @author Ragnos
+ */
+public class zimmer {
+    
+    boolean avail = true;
+    int cBetten;
+    int znr;
+        
+    public zimmer(){
+        
+    }
+    
+    public zimmer(int nZnr, int nBetten){
+        cBetten = nBetten;
+        znr = nZnr;
+    }
+}
