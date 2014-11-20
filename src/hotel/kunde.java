d15ea89b156905403c1510d858c3b428ccccaa44
ragnos <ragnos@utzone.de>
2014-11-20 18:19:45 +0100

    Kunden-Klasse

---
+++ src/hotel/kunde.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotel;
+
+/**
+ *
+ * @author Ragnos
+ */
+public class kunde {
+    int kdnr;
+    String name, adresse;
+    
+    public kunde(){
+        /* todo */
+    }
+    
+    public kunde(String n, String adr){
+        name = n;
+        adresse = adr;
+    }
+}
