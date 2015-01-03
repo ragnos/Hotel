08a44eab69b786a3a4a878225707a875a2798a1e
ragnos <ragnos@utzone.de>
2015-01-03 20:34:49 +0100

    joda-time ist raus, LocalDate ist drin! Anfrage-Plausibilitätscheck Anfrage zu Reservierung

--- src/hotel/Hotel.java
+++ src/hotel/Hotel.java
@@ -5,6 +5,9 @@
  */
 package hotel;
 
+import java.util.*;
+import java.time.*;
+
 /**
  *
  * @author Ragnos
@@ -15,7 +18,25 @@ public class Hotel {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        List<reservierung> ResList = new ArrayList<>();
+        Iterator it = ResList.iterator();
+        
+        //Demodaten: Kunden:
+        kunde kunde_a = new kunde("Andreas A.","Auf der Flucht 1");
+        kunde kunde_b = new kunde("Berta B.","Bauerngasse 2");
+        kunde kunde_c = new kunde("Christopher C.","Clausberger Allee 3");
+
+        //Demodaten: Reservierungen
+        ResList.add(new reservierung(1,LocalDate.of(2015, Month.JANUARY, 1),LocalDate.of(2015, Month.JANUARY, 15),1));
+        ResList.add(new reservierung(2,LocalDate.of(2015, Month.JANUARY, 16),LocalDate.of(2015, Month.FEBRUARY, 1),1));
+        ResList.add(new reservierung(3,LocalDate.of(2015, Month.JANUARY, 1),LocalDate.of(2015, Month.FEBRUARY, 2),2));
+        
+        anfrage a1 = new anfrage(LocalDate.of(2015,Month.FEBRUARY,1), LocalDate.of(2015,Month.FEBRUARY,15), 1);
+        
+        /*Das hier ist noch "suboptimal"... Man müsste durchgängig vorgenerierte
+        anfrage-Objekte verwenden, sonst wären sie überflüssig. Ich lass erstmal
+        beides drin... */
+        if(kunde_a.anfrageZimmerFrei(LocalDate.of(2015,Month.FEBRUARY,1), LocalDate.of(2015,Month.FEBRUARY,15), 1, ResList))
+            kunde_a.anfrageReservieren(a1,ResList);
+        }
 }
